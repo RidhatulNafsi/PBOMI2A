119df49507017973c7c000909477efa1d7e10fc0
RidhatulNafsi <114200748+RidhatulNafsi@users.noreply.github.com>
2023-05-31 12:28:58 +0700

    First Commit

---
+++ src/BankATM_RidhatulNafsi_2101092066/InformasiSaldoModel.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BankATM_RidhatulNafsi_2101092066;
+import BankATM_RidhatulNafsi_2101092066.TransferModel;
+
+/**
+ *
+ * @author ridha
+ */
+public class InformasiSaldoModel {
+    String saldo="90000000";
+    
+
+    public String getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(String saldo) {
+        this.saldo = saldo;
+    }
+    
+}
---
+++ src/BankATM_RidhatulNafsi_2101092066/PinModel.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BankATM_RidhatulNafsi_2101092066;
+
+/**
+ *
+ * @author ridha
+ */
+public class PinModel {
+   
+    String pin = "123456";
+
+    public String getPin() {
+        return pin;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+
+    
+    
+    
+}
